f5dfb317552c8c0510bc4a8ced14e60e28939408
Segun Konibire <gunzalez@hotmail.com>
2016-12-05 21:22:25 +0000

    Adds footer

--- js/valtech_.js
+++ js/valtech_.js
@@ -4,33 +4,51 @@
 
     var onboarding = {};
 
+    onboarding.fun = {
+        switchBios: function(){
+            var $bio = $('.bio'),
+                rdnNumber = Math.floor((Math.random() * $bio.length) + 1);
+            $bio.removeClass('remove');
+            $bio.not($bio.eq(rdnNumber-1)).addClass('remove');
+        }
+    };
+
     onboarding.init = function(){
 
         var $logoutButtons = $('.logout-button');
         $logoutButtons.on('click', function(evt){
             evt.preventDefault();
+            // do some ajax to log user out
+            //
             var destination = $(this).attr('href');
             $.mobile.navigate(destination);
         });
-
-        var $nameLinks = $('#aListOfNames a');
-        $nameLinks.on('click', function(evt){
-            evt.preventDefault();
-            $('.personName').text($(this).text());
-            var destination = $(this).attr('href');
-            $.mobile.navigate(destination);
-        });
-
 
         var $signInForm = $('#sign-in-form');
         $signInForm.on('submit', function(){
+            // do some ajax to log user in
+            //
             var destination = $(this).attr('action');
             $.mobile.navigate(destination);
             return false;
         });
 
+        var $nameLinks = $('#aListOfNames a');
+        $nameLinks.on('click', function(evt){
+
+            evt.preventDefault();
+            $('.personName').text($(this).text());
+
+            // just some fun
+            onboarding.fun.switchBios();
+
+            var destination = $(this).attr('href');
+            $.mobile.navigate(destination);
+        });
+
         var $peopleForm = $('#people-form');
         $peopleForm.on('submit', function(){
+
             var destination = $(this).attr('action'),
             skills = $('#people-capabilities',$(this)).val(),
             client = $('#people-client',$(this)).val(),
@@ -53,7 +71,7 @@
                 terms.push('<strong>' + location.replace(' ', '&nbsp;') + '</strong>');
             }
 
-            if(cop != 'Community Of Practice'){
+            if(cop != 'Community of Practice'){
                 terms.push('<strong>' + cop.replace(' ', '&nbsp;') + '</strong>');
             }
 
@@ -66,6 +84,7 @@
                 termsInWords = terms[0] + '.';
             }
 
+            // create list of search strings, to display on next screen
             if(termsInWords != ''){
                 $('#resultsFromPeople').empty().append(termsInWords);
                 $('#listOfNames').parent().find('a').trigger('click'); // empties the filter
@@ -74,6 +93,8 @@
             $.mobile.navigate(destination);
             return false;
         });
+
+        onboarding.fun.switchBios();
     };
 
     $(document).ready(function () {
